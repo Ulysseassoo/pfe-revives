bb40ac4c3698a5a1781b96f51abef624d64ec06e
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-04-05 22:54:32 +0200

    Fixed database errors

--- prisma/migrations/20230405205102_auto_increment/migration.sql
+++ prisma/migrations/20230405205102_auto_increment/migration.sql
@@ -1,4 +1,82 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `user_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(255) NULL,
+    `last_name` VARCHAR(255) NULL,
+    `email` VARCHAR(191) NULL,
+    `password` VARCHAR(255) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `role` INTEGER NOT NULL,
+    `stripe_id` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Shoe` (
+    `shoe_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `brand` VARCHAR(191) NULL,
+    `model` VARCHAR(191) NULL,
+    `colorway` VARCHAR(191) NULL,
+    `size` DOUBLE NULL,
+    `status` VARCHAR(191) NULL,
+    `price` DOUBLE NULL,
+    `description` VARCHAR(191) NULL,
+    `user_id` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `is_validate` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`shoe_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Photo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image_url` VARCHAR(191) NULL,
+    `shoes_shoe_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Buyer` (
+    `buyer_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Buyer_user_id_key`(`user_id`),
+    PRIMARY KEY (`buyer_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CollectionPoint` (
+    `collection_point_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `address_line_1` VARCHAR(191) NULL,
+    `address_line_2` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NULL,
+    `state` VARCHAR(191) NULL,
+    `zip_code` VARCHAR(191) NULL,
+    `country` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `points` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`collection_point_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Forum` (
+    `post_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `title` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`post_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `ShippingOption` (
     `shipping_option_id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NULL,
@@ -35,7 +113,7 @@ CREATE TABLE `Return` (
 
 -- CreateTable
 CREATE TABLE `Seller` (
-    `seller_id` INTEGER NOT NULL,
+    `seller_id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
@@ -44,15 +122,15 @@ CREATE TABLE `Seller` (
 
 -- CreateTable
 CREATE TABLE `ShippingAddress` (
-    `shipping_address_id` INTEGER NOT NULL,
+    `shipping_address_id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NULL,
-    `full_name` VARCHAR(191) NULL,
-    `address_line_1` VARCHAR(191) NULL,
-    `address_line_2` VARCHAR(191) NULL,
-    `city` VARCHAR(191) NULL,
-    `state` VARCHAR(191) NULL,
-    `zip_code` VARCHAR(191) NULL,
-    `country` VARCHAR(191) NULL,
+    `full_name` VARCHAR(255) NULL,
+    `address_line_1` VARCHAR(255) NULL,
+    `address_line_2` VARCHAR(255) NULL,
+    `city` VARCHAR(255) NULL,
+    `state` VARCHAR(255) NULL,
+    `zip_code` VARCHAR(255) NULL,
+    `country` VARCHAR(255) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`shipping_address_id`)
@@ -94,6 +172,38 @@ CREATE TABLE `Comment` (
     PRIMARY KEY (`users_user_id`, `shoes_shoe_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Message` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `text` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `sender_id` INTEGER NOT NULL,
+    `recipient_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_CollectionPointToShoe` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_CollectionPointToShoe_AB_unique`(`A`, `B`),
+    INDEX `_CollectionPointToShoe_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Shoe` ADD CONSTRAINT `Shoe_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Photo` ADD CONSTRAINT `Photo_shoes_shoe_id_fkey` FOREIGN KEY (`shoes_shoe_id`) REFERENCES `Shoe`(`shoe_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Buyer` ADD CONSTRAINT `Buyer_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Forum` ADD CONSTRAINT `Forum_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE `Order` ADD CONSTRAINT `Order_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -132,3 +242,15 @@ ALTER TABLE `Comment` ADD CONSTRAINT `Comment_users_user_id_fkey` FOREIGN KEY (`
 
 -- AddForeignKey
 ALTER TABLE `Comment` ADD CONSTRAINT `Comment_shoes_shoe_id_fkey` FOREIGN KEY (`shoes_shoe_id`) REFERENCES `Shoe`(`shoe_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_sender_id_fkey` FOREIGN KEY (`sender_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_recipient_id_fkey` FOREIGN KEY (`recipient_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CollectionPointToShoe` ADD CONSTRAINT `_CollectionPointToShoe_A_fkey` FOREIGN KEY (`A`) REFERENCES `CollectionPoint`(`collection_point_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CollectionPointToShoe` ADD CONSTRAINT `_CollectionPointToShoe_B_fkey` FOREIGN KEY (`B`) REFERENCES `Shoe`(`shoe_id`) ON DELETE CASCADE ON UPDATE CASCADE;
